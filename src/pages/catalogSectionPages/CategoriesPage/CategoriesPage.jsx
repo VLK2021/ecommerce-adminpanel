e4d6a4914de386f3ea4ee182bd3a3ad56a394b6e
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-06-03 22:27:57 +0300

    commit

---
+++ src/pages/catalogSectionPages/CategoriesPage/CategoriesPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import css from './CategoriesPage.module.css';
+
+
+const CategoriesPage = () => {
+    return (
+        <div className={css.wrap}>
+            CategoriesPage
+        </div>
+    );
+};
+
+export {CategoriesPage};
---
+++ src/pages/catalogSectionPages/ProductPage/ProductPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import css from './ProductPage.module.css';
+
+
+const ProductPage = () => {
+    return (
+        <div className={css.wrap}>
+            ProductPage
+        </div>
+    );
+};
+
+export {ProductPage};
